0fa7975d73feb3433356a62f91f109da6350ea48
Sameer Hasan <simplysamy@gmail.com>
2024-11-28 17:55:05 +0530

    Add encoding

--- 4_rag/2a_rag_basics_metadata.py
+++ 4_rag/2a_rag_basics_metadata.py
@@ -31,7 +31,7 @@ if not os.path.exists(persistent_directory):
     documents = []
     for book_file in book_files:
         file_path = os.path.join(books_dir, book_file)
-        loader = TextLoader(file_path)
+        loader = TextLoader(file_path, encoding="utf-8")
         book_docs = loader.load()
         for doc in book_docs:
             # Add metadata to each document indicating its source
