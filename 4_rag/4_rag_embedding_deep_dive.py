d7ef2f46a9c5112cc36e5ec4dffd724f76d0a0ba
Sameer Hasan <simplysamy@gmail.com>
2024-11-30 15:16:56 +0530

    Add utf -8 encoding

--- 4_rag/4_rag_embedding_deep_dive.py
+++ 4_rag/4_rag_embedding_deep_dive.py
@@ -18,7 +18,7 @@ if not os.path.exists(file_path):
     )
 
 # Read the text content from the file
-loader = TextLoader(file_path)
+loader = TextLoader(file_path, encoding="utf-8")
 documents = loader.load()
 
 # Split the document into chunks
